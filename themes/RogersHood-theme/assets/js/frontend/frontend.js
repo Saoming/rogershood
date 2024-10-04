64a72915814db0d496a40ce7485415bf885408d7
Samuel Tirtawidjaja <samueltirtawidjaja@gmail.com>
2024-10-05 03:22:51 +0700

    fix emi code

--- themes/RogersHood-theme/assets/js/frontend/frontend.js
+++ themes/RogersHood-theme/assets/js/frontend/frontend.js
@@ -1,8 +1,22 @@
 import '../../css/frontend/style.css';
 
 import cart from './components/cart';
-import Blocks from "./blocks/blocks";
+import Blocks from './blocks/blocks';
 
+document.addEventListener('DOMContentLoaded', function () {
+	const cards = document.querySelectorAll('.community-card');
+	// prettier-ignore
+	cards.forEach(card => {
+		card.addEventListener('mouseover', () => {
+			cards.forEach(c => c.classList.remove('is-active'));
+			card.classList.add('is-active');
+		});
+
+		card.addEventListener('mouseout', () => {
+			card.classList.remove('is-active');
+		});
+	});
+});
 
 cart();
 Blocks();
