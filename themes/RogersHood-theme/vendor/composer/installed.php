fc8f2bd41c038240f652ee0b43d4ba7b2c5d4a6c
Samuel Tirtawidjaja <samueltirtawidjaja@gmail.com>
2024-10-05 03:28:12 +0700

    fix for emi

--- themes/RogersHood-theme/vendor/composer/installed.php
+++ themes/RogersHood-theme/vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-main',
-    'version' => 'dev-main',
-    'aliases' => 
-    array (
-    ),
-    'reference' => 'afd158864645fdd738d54c480e1eb5bb369cd751',
-    'name' => '10up/wp-theme',
-  ),
-  'versions' => 
-  array (
-    '10up/wp-theme' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'afd158864645fdd738d54c480e1eb5bb369cd751',
-    ),
-    'haydenpierce/class-finder' => 
-    array (
-      'pretty_version' => '0.4.4',
-      'version' => '0.4.4.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '94c602870ddf8d4fa2d67fb9bae637d88f9bd76e',
-    ),
-  ),
+<?php return array(
+	'root'     => array(
+		'name'           => '10up/wp-theme',
+		'pretty_version' => 'dev-master',
+		'version'        => 'dev-master',
+		'reference'      => '383ce7578b4d976bfa8994ac7d6ac7c24d2d67d0',
+		'type'           => 'wordpress-theme',
+		'install_path'   => __DIR__ . '/../../',
+		'aliases'        => array(),
+		'dev'            => true,
+	),
+	'versions' => array(
+		'10up/wp-theme'             => array(
+			'pretty_version'  => 'dev-master',
+			'version'         => 'dev-master',
+			'reference'       => '383ce7578b4d976bfa8994ac7d6ac7c24d2d67d0',
+			'type'            => 'wordpress-theme',
+			'install_path'    => __DIR__ . '/../../',
+			'aliases'         => array(),
+			'dev_requirement' => false,
+		),
+		'haydenpierce/class-finder' => array(
+			'pretty_version'  => '0.4.4',
+			'version'         => '0.4.4.0',
+			'reference'       => '94c602870ddf8d4fa2d67fb9bae637d88f9bd76e',
+			'type'            => 'library',
+			'install_path'    => __DIR__ . '/../haydenpierce/class-finder',
+			'aliases'         => array(),
+			'dev_requirement' => false,
+		),
+	),
 );
