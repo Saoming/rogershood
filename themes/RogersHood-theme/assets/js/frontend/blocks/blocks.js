ee0d2d9cfe30e5b5505551d26e860e39aa180a52
Tome Pajkovski <tome.pajkovski@devvironment.com>
2024-10-04 16:41:41 +0200

    finishing the faq block

---
+++ themes/RogersHood-theme/assets/js/frontend/blocks/blocks.js
@@ -0,0 +1,7 @@
+import Faq from "./faq";
+
+const Blocks = () =>  {
+	Faq();
+}
+
+export default Blocks;
--- themes/RogersHood-theme/partials/blocks/faq/faq.php
+++ themes/RogersHood-theme/partials/blocks/faq/faq.php
@@ -8,62 +8,79 @@ $description = get_field( 'intro_description' );
 $image       = get_field( 'intro_image' );
 $cta         = get_field( 'intro_cta' );
 
-$alignment_class        = get_field( 'alignment' ) ? 'text-center' : '';
+$alignment_class        = get_field( 'text_align' ) ? 'text-center' : '';
 $background_color_class = get_field( 'background_color' ) ? 'bg-blue' : '';
 
 if ( $cta ) {
-	$cta_text = isset( $cta['text'] ) ? $cta['text'] : '';
+	$cta_text = isset( $cta['title'] ) ? $cta['title'] : '';
 	$cta_link = isset( $cta['url'] ) ? $cta['url'] : '';
+	$cta_target = isset( $cta['target'] ) ? $cta['target'] : '';
 }
+
+
 ?>
 
 
-<div class="fs-block faq-block">
+<div class="rh-block faq-block <?php echo esc_attr("$background_color_class"); ?>">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 col-lg-5">
-				<div class="faq-intro__inner">
+				<div class="faq-block__intro__inner <?php echo esc_attr("$alignment_class"); ?>">
 					<?php if ( $title ) { ?>
-						<h2 class="faq-intro__title"><?php echo esc_html( $title ); ?></h2>
+						<h2 class="faq-block__intro__title"><?php echo esc_html( $title ); ?></h2>
 					<?php }
 					if ( $description ) { ?>
-						<div class="faq-intro__description"><?php echo wp_kses_post( $description ); ?></div>
-					<?php }
-					if ( $image ) { ?>
-						<div class="faq-intro__image">
-							<?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
-						</div>
+						<div class="faq-block__intro__description text-body-20"><?php echo wp_kses_post( $description ); ?></div>
 					<?php }
 					if ( $cta ) { ?>
-						<div class="faq-intro__cta">
-							<a href="<?php echo esc_url( $cta_link ); ?>"
+						<div class="faq-block__intro__cta button-primary">
+							<a href="<?php echo esc_url( $cta_link ); ?>" <?php echo esc_attr( $cta_target ); ?>
 							   class="btn btn-primary"><?php echo esc_html( $cta_text ); ?></a>
+						</div>
+					<?php }
+					if ( $image ) { ?>
+						<div class="faq-block__intro__image">
+							<?php echo wp_get_attachment_image( $image, 'full' ); ?>
 						</div>
 					<?php } ?>
 				</div>
 			</div>
 			<div class="col-md-6 col-lg-7">
-				<?php
-				if(have_rows('question_section')) : while(have_rows('question_section')) : the_row();
-						$section_title = get_sub_field('section_title');
-						if($section_title) {
-							echo '<h3 class="faq-section-title">' . $section_title . '</h3>';
-						}
-						if(have_rows('questions')) : while(have_rows('questions')) : the_row();
-							$question = get_sub_field('question_content');
-							$answer = get_sub_field('answer_content');
-							if($question && $answer) {
-								echo '<div class="faq-question">';
-								echo '<h4 class="faq-question__title">' . $question . '</h4>';
-								echo '<div class="faq-question__answer">' . $answer . '</div>';
-								echo '</div>';
+				<div class="faq-block__questions-container">
+					<?php
+					if ( have_rows( 'question_section' ) ) : while ( have_rows( 'question_section' ) ) : the_row();
+						$section_title = get_sub_field( 'section_title' );
+						?>
+						<div class="faq-block__questions-section">
+							<?php
+							if ( $section_title ) {
+								echo '<h3 class="faq-block__section-title">' . $section_title . '</h3>';
 							}
+							if ( have_rows( 'questions' ) ) : while ( have_rows( 'questions' ) ) : the_row();
+								$question = get_sub_field( 'question_content' );
+								$answer   = get_sub_field( 'answer_content' );
+								if ( $question && $answer ) {
+									?>
+									<div class="faq-block__question">
+										<h4 class="faq-block__question-title"><?php echo wp_kses_post( $question ); ?>
+											<span class="faq-block__question-icon"></span>
+										</h4>
+
+										<div class="faq-block__question-answer">
+											<div class="faq-block__question-answer--inner">
+												<?php echo wp_kses_post( $answer ); ?></div>
+
+										</div>
+									</div>
+									<?php
+								}
 							endwhile;
 							endif;
-				?>
-				<?php endwhile;  endif; ?>
+							?>
+						</div>
+					<?php endwhile; endif; ?>
+				</div>
 			</div>
-
 		</div>
 	</div>
 </div>
