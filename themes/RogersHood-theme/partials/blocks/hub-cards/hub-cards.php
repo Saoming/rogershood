72086187ff580c11fdc4a40a7d35c4f4ca25055f
Emilija Pajkovska <emipajk@namiko.com.mk>
2024-10-06 06:37:45 +0200

    Hub cards ready for testing after button class provided

--- themes/RogersHood-theme/partials/blocks/hub-cards/hub-cards.php
+++ themes/RogersHood-theme/partials/blocks/hub-cards/hub-cards.php
@@ -23,12 +23,10 @@ echo '</pre>';
 <section class="rh-block  hub-cards"
 		 id="<?php echo esc_attr( $id ); ?>"
 		 style="background: <?php echo esc_attr( $fields["background_color"] ); ?> ">
-
-	>
 	<div class="container container--narrow">
-		<div class="hub-cards__top text-center mb-50">
+		<div class="hub-cards__top text-center mb-50 max-width-headings">
 			<?php if ( $fields["pretitle"] ) { ?>
-				<div class="hub-cards__pretitle fw-300 uppercase fs-16">
+				<div class="hub-cards__pretitle fw-300 uppercase fs-16 mb-16">
 					<?php echo esc_attr( $fields["pretitle"] ); ?>
 				</div>
 			<?php } ?>
@@ -38,7 +36,7 @@ echo '</pre>';
 				</h2>
 			<?php } ?>
 			<?php if ( $fields["description"] ) { ?>
-				<div class="hub-cards__description">
+				<div class="hub-cards__description fs-20 fw-300">
 					<?php echo esc_attr( $fields["description"] ); ?>
 				</div>
 			<?php } ?>
@@ -46,36 +44,34 @@ echo '</pre>';
 		<div class="container--inner">
 			<div class="hub-card__row row">
 				<?php foreach ( $fields["cards"] as $cards ) { ?>
-				<div class="hub-card cols-sm-12  col-md-4 br-4">
-				<div class="hub-card__background_image__container">
-					<?php echo wp_get_attachment_image( $cards["background_image"], 'full', null, array( 'class' => 'hub-card__background_image br-4' ) ); ?>
-				</div>
-				<div class="hub-card__content__inner">
-					<div class="hub-card__content__inner__top">
-						<?php if ( $cards["icon"] ) { ?>
-							<!--								<div class=" hub-card__icon-container mb-20">-->
-							<?php echo wp_get_attachment_image( $cards["icon"], '', null, array( 'class' => 'hub-card__icon  mb-20 br-12' ) ); ?>
-							<!--								</div>-->
-						<?php } ?>
-						<?php if ( $cards["title"] ) { ?>
-							<div class="hub-card__title fs-32  ff-scilla mb-20"><?php echo wp_kses_post( $cards["title"] ); ?></div>
-						<?php } ?>
-						<?php if ( $cards["description"] ) { ?>
-							<div class="hub-card__title fw-300"><?php echo wp_kses_post( $cards["description"] ); ?></div>
-						<?php } ?>
-					</div>
-					<?php if ( $cards["cta"] ) { ?>
-						<div class="temp-button hub-card__cta">
-							<a class="temp-button hub-card__button"
-							   href="<?php echo esc_url( $cards["cta"]["url"] ); ?>"
-							   target="<?php echo esc_attr( $cards["cta"]["target"] ); ?>"><?php echo esc_html( $cards["cta"]["title"] ); ?></a>
+					<div class="hub-card cols-sm-12  col-md-4 br-4">
+						<div class="hub-card__background_image__container">
+							<?php echo wp_get_attachment_image( $cards["background_image"], 'full', null, array( 'class' => 'hub-card__background_image br-4' ) ); ?>
 						</div>
-					<?php } ?>
+						<div class="hub-card__content__inner">
+							<div class="hub-card__content__inner__top">
+								<?php if ( $cards["icon"] ) { ?>
+									<?php echo wp_get_attachment_image( $cards["icon"], 'thumbnail', null, array( 'class' => 'hub-card__icon  mb-20 br-12' ) ); ?>
+								<?php } ?>
+								<?php if ( $cards["title"] ) { ?>
+									<div class="hub-card__title fs-32  ff-scilla mb-20"><?php echo wp_kses_post( $cards["title"] ); ?></div>
+								<?php } ?>
+								<?php if ( $cards["description"] ) { ?>
+									<div class="hub-card__title fw-300"><?php echo wp_kses_post( $cards["description"] ); ?></div>
+								<?php } ?>
+							</div>
+							<?php if ( $cards["cta"] ) { ?>
+								<div class="hub-card__cta">
+									<a class="button hub-card__button"
+									   href="<?php echo esc_url( $cards["cta"]["url"] ); ?>"
+									   target="<?php echo esc_attr( $cards["cta"]["target"] ); ?>"><?php echo esc_html( $cards["cta"]["title"] ); ?></a>
+								</div>
+							<?php } ?>
 
-				</div>
+						</div>
+					</div>
+				<?php } ?>
 			</div>
-			<?php } ?>
 		</div>
-	</div>
 	</div>
 </section>
