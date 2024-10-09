bfe98f5b2957314440ed643957691ecf21826e57
Samuel Tirtawidjaja <samueltirtawidjaja@gmail.com>
2024-10-09 21:48:46 +0700

    fix carousel design with switch case

--- themes/RogersHood-theme/assets/js/frontend/components/carousel.js
+++ themes/RogersHood-theme/assets/js/frontend/components/carousel.js
@@ -7,22 +7,37 @@ class SplideCarousel {
 		const elms = document.getElementsByClassName('splide');
 
 		for (let i = 0; i < elms.length; i++) {
-			// const { AutoScroll } = window.splide.Extensions;
 			// eslint-disable-next-line no-undef
-			const splide = new Splide(elms[i]).mount();
+			const splide = new Splide(elms[i]);
 			const carouselID = splide.root.id;
+			const { AutoScroll } = window.splide.Extensions;
+			// eslint-disable-next-line no-undef
+			const primary = new Splide('#testimonialsCarousel');
+			// eslint-disable-next-line no-undef
+			const secondary = new Splide('#testimonialPersonSlider');
 			const btnPrev = document.querySelector('.testimonial-arrow-prev');
 			const btnNext = document.querySelector('.testimonial-arrow-next');
 
-			if (carouselID === 'testimonialsCarousel' || carouselID === 'testimonialPersonSlider') {
-				btnPrev.addEventListener('click', (e) => {
-					console.log(e.target);
-					splide.go('-1');
-				});
-				btnNext.addEventListener('click', (e) => {
-					console.log(e.target);
-					splide.go('+1');
-				});
+			switch (carouselID) {
+				case 'testimonialsCarousel':
+				case 'testimonialPersonSlider':
+					primary.sync(secondary);
+					primary.mount();
+					secondary.mount();
+
+					btnPrev.addEventListener('click', () => {
+						primary.go('-1');
+					});
+					btnNext.addEventListener('click', () => {
+						primary.go('+1');
+					});
+					break;
+				case 'headerBanner':
+					splide.mount({ AutoScroll });
+					break;
+				default:
+					splide.mount();
+					break;
 			}
 		}
 	}
