48f5d208a4666b5f51ec36fdebf2b94c2071e501
Emilija Pajkovska <emipajk@namiko.com.mk>
2024-10-11 13:54:11 +0200

    Subscribe form in the footer

--- themes/RogersHood-theme/partials/blocks/text-and-gform/text-and-gform.php
+++ themes/RogersHood-theme/partials/blocks/text-and-gform/text-and-gform.php
@@ -52,10 +52,8 @@ if ( ! get_field( 'block_preview' ) ) {
 				<div class="col-sm-hidden col-md-1"></div>
 				<div class="col-md-6 text-and-gform__gform">
 					<?php
-					if ( function_exists( 'gravity_form' ) ) {
 						$gravity_shortcode = '[gravityform id="' . $fields["gravity_form"] . '" title="false" description="false" ajax="true"]';
 						echo do_shortcode( $gravity_shortcode );
-					}
 					?>
 				</div>
 			</div>
--- themes/RogersHood-theme/partials/footer/footer-view.php
+++ themes/RogersHood-theme/partials/footer/footer-view.php
@@ -1,4 +1,11 @@
 <footer role="contentinfo" class="footer__container page-container">
+	<div class="footer_subscribe__container show-on-mobile">
+		<h3 class="footer_subscribe_heading">Join Our Hood</h3>
+		<p class="footer_subscribe_text">Promos, product launches, and events delivered straight to your inbox</p>
+		<div class="footer_subscribe_form">
+			<?php echo do_shortcode( '[gravityform name="Subscribe" title="false" description="false" ajax="true"]' ); ?>
+		</div>
+	</div>
 	<div class="footer_menu__container">
 		<div class="footer_menu__container_columns">
 			<h3 class="footer_menu_heading">Shop</h3>
@@ -19,9 +26,12 @@
 	</div>
 
 	<div class="footer_simple_content__container">
-		<div class="footer_subscribe__container">
-			<h3 class="footer_subscribe_heading">Subscribe</h3>
+		<div class="footer_subscribe__container show-on-desktop">
+			<h3 class="footer_subscribe_heading">Join Our Hood</h3>
 			<p class="footer_subscribe_text">Promos, product launches, and events delivered straight to your inbox</p>
+			<div class="footer_subscribe_form">
+				<?php echo do_shortcode( '[gravityform name="Subscribe" title="false" description="false" ajax="true"]' ); ?>
+			</div>
 		</div>
 		<div class="footer_disclaimer__container">
 			<h3 class="footer_disclaimer_heading">Disclaimer</h3>
