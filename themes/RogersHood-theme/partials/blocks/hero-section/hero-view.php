ae5972c071983dab6863b7c34d033d0d27967c48
Samuel Tirtawidjaja <samueltirtawidjaja@gmail.com>
2024-10-12 02:14:37 +0700

    hero-section fix mobile

--- themes/RogersHood-theme/partials/blocks/hero-section/hero-view.php
+++ themes/RogersHood-theme/partials/blocks/hero-section/hero-view.php
@@ -1,6 +1,6 @@
 <section class="hero-container rh-block bg-blue" id="<?php echo esc_attr( $args['id'] ); ?>" style="background: url('<?php echo esc_url( $args['hero_background']['url'] ); ?>')">
 				<div class="hero-inner row">
-					<div class="col-sm-12 col-lg-6 text-center">
+					<div class="col-sm-12 col-lg-6 text-center hero-inner-content">
 						<h1 class="hero-section-final-title"><?php echo esc_attr( $args['hero_heading'] ); ?></h1>
 						<div class="wysig-video__container">
 							<?php echo wp_kses_post( $args['hero_description'] ); ?>
