8f8154f1c502abd818545078e4caa3d16fd26e3c
Samuel Tirtawidjaja <samueltirtawidjaja@gmail.com>
2024-10-15 05:59:48 +0700

    fix WooCommerce link in title

--- themes/RogersHood-theme/includes/woocommerce.php
+++ themes/RogersHood-theme/includes/woocommerce.php
@@ -2,7 +2,9 @@
 
 defined( 'ABSPATH' ) || exit;
 
-if ( ! in_array( 'woocommerce/woocommerce.php', rh_get_active_plugins(), true ) ) return;
+if ( ! in_array( 'woocommerce/woocommerce.php', rh_get_active_plugins(), true ) ) {
+	return;
+}
 
 /**
  * @return int
@@ -11,12 +13,15 @@ function rh_get_shop_page_id() {
 
 	static $id;
 
-	if ( isset( $id ) ) return $id;
+	if ( isset( $id ) ) {
+		return $id;
+	}
 
 	$id = \function_exists( 'wc_get_page_id' ) ? (int) \wc_get_page_id( 'shop' ) : 0;
 
-	if ( \get_post( $id ) )
+	if ( \get_post( $id ) ) {
 		return $id;
+	}
 
 	return $id = 0;
 }
@@ -55,16 +60,20 @@ add_filter( 'woocommerce_demo_store', 'modify_woocommerce_demo_store', 10, 2 );
 function custom_pre_get_posts_query( $q ) {
 
 	// Only modify the shop's main query.
-	if ( ! rh_is_shop() || ! $q->is_main_query() ) return;
+	if ( ! rh_is_shop() || ! $q->is_main_query() ) {
+		return;
+	}
 
 	// Get all WooCommerce product categories
-	$terms = get_terms( [
-		'taxonomy'   => 'product_cat',
-		'hide_empty' => false,
-	] );
+	$terms = get_terms(
+		array(
+			'taxonomy'   => 'product_cat',
+			'hide_empty' => false,
+		)
+	);
 
 	// Filter the terms to get the IDs of the excluded categories
-	$excluded_cats    = array_filter( $terms, fn( $term ) => in_array( $term->slug, [ 'ebooks', 'uncategorized' ], true ) );
+	$excluded_cats    = array_filter( $terms, fn( $term ) => in_array( $term->slug, array( 'ebooks', 'uncategorized' ), true ) );
 	$excluded_cat_ids = array_map( fn( $term ) => $term->term_id, $excluded_cats );
 
 	// Exclude the categories from the query
@@ -73,12 +82,12 @@ function custom_pre_get_posts_query( $q ) {
 	// Set to 'AND' if not set.
 	$tax_query['relation'] ??= 'AND';
 
-	$tax_query[] = [
+	$tax_query[] = array(
 		'taxonomy' => 'product_cat',
 		'field'    => 'term_id',
 		'terms'    => $excluded_cat_ids,
 		'operator' => 'NOT IN',
-	];
+	);
 
 	$q->set( 'tax_query', $tax_query );
 }
@@ -92,4 +101,15 @@ function wcc_change_breadcrumb_delimiter( $defaults ) {
 	// Change the breadcrumb delimeter from '/' to '>'
 	$defaults['delimiter'] = ' &gt; ';
 	return $defaults;
+}
+
+/**
+ * Change the breadcrumb separator
+ */
+remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
+add_filter( 'woocommerce_shop_loop_item_title', 'wc_link_fix', 10 );
+function wc_link_fix() {
+	echo '<a href=" ' . get_the_permalink() . '">';
+	echo '<h2 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '">' . get_the_title() . '</h2>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo '</a>';
 }
