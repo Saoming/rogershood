e5be331a04d5cb2072c2f55a05524e8f9a44276e
Samuel Tirtawidjaja <samueltirtawidjaja@gmail.com>
2024-11-02 00:54:47 +0700

    fix gravity form

--- themes/RogersHood-theme/partials/blocks/text-and-gform/text-and-gform.php
+++ themes/RogersHood-theme/partials/blocks/text-and-gform/text-and-gform.php
@@ -22,38 +22,38 @@ if ( ! get_field( 'block_preview' ) ) {
 			<div class="text-and-gform__row row">
 
 				<div class="col-md-5 text-and-gform__content text-center">
-					<?php if ( $fields["pretitle"] ) { ?>
+					<?php if ( $fields['pretitle'] ) { ?>
 						<div class="text-and-gform__pretitle pretitle ">
-							<?php echo esc_attr( $fields["pretitle"] ); ?>
+							<?php echo esc_attr( $fields['pretitle'] ); ?>
 						</div>
 						<?php
 					}
-					if ( $fields["title"] ) {
+					if ( $fields['title'] ) {
 						?>
 						<h2 class="text-and-gform__title">
-							<?php echo esc_attr( $fields["title"] ); ?>
+							<?php echo esc_attr( $fields['title'] ); ?>
 						</h2>
 						<?php
 					}
-					if ( $fields["description"] ) {
+					if ( $fields['description'] ) {
 						?>
 						<div
-								class="text-and-gform__description mb-50"><?php echo wp_kses_post( $fields["description"] ); ?></div>
+								class="text-and-gform__description mb-50"><?php echo wp_kses_post( $fields['description'] ); ?></div>
 						<?php
 					}
-					if ( $fields["image"] ) {
-						echo wp_get_attachment_image( $fields["image"], 'full', null, array( 'class' => 'text-and-gform__image' ) );
+					if ( $fields['image'] ) {
+						echo wp_get_attachment_image( $fields['image'], 'full', null, array( 'class' => 'text-and-gform__image' ) );
 					} else {
 						?>
 						<img class="missing-image text-and-gform__image"
-							 src="<?php echo esc_url( TENUP_THEME_DIST_URL . '/images/missing-image.png' ); ?>">
+							src="<?php echo esc_url( TENUP_THEME_DIST_URL . '/images/missing-image.png' ); ?>">
 					<?php } ?>
 				</div>
 				<div class="col-sm-hidden col-md-1"></div>
 				<div class="col-md-6 text-and-gform__gform">
 					<?php
-						// $gravity_shortcode = '[gravityform id="' . $fields["gravity_form"] . '" title="false" description="false" ajax="true"]';
-						// echo do_shortcode( $gravity_shortcode );
+						$gravity_shortcode = '[gravityform id="' . $fields['gravity_form'] . '" title="false" description="false" ajax="true"]';
+						echo do_shortcode( $gravity_shortcode );
 					?>
 				</div>
 			</div>
@@ -61,6 +61,6 @@ if ( ! get_field( 'block_preview' ) ) {
 	</section>
 <?php } else { ?>
 	<div data="gutenberg-preview-img">
-		<img style="max-width:100%; height:auto;" src="<?php the_field( 'block_preview' ) ?>"/>
+		<img style="max-width:100%; height:auto;" src="<?php the_field( 'block_preview' ); ?>"/>
 	</div>
 <?php } ?>
